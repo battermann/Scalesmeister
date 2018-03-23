299385ae4de3b9b3aa57afd7284567facc4451f7
Leif Battermann <leif.battermann@gmail.com>
2018-03-23 22:12:36 +0100

    initial

---
+++ App.elm
@@ -0,0 +1,26 @@
+import Html exposing (beginnerProgram, div, button, text)
+import Html.Events exposing (onClick)
+
+
+main =
+  beginnerProgram { model = 0, view = view, update = update }
+
+
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
+
+
+type Msg = Increment | Decrement
+
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
