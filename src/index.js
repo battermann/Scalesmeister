08fa213809dfbee4f0737ab17bd2401babe690dc
Leif Battermann <leif.battermann@gmail.com>
2018-10-07 11:31:50 +0200

    fontawesome as npm package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "@fortawesome/fontawesome-free/css/all.css"
 import { Elm } from './Main.elm'
 import { Sampler, Transport, Part, Player } from 'tone'
 import StartAudioContext from 'startaudiocontext'
