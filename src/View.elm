4ad5d67a86645c212d62fd4d0bf755c465f54908
Leif Battermann <leif.battermann@gmail.com>
2019-05-04 00:31:48 +0200

    version bump

--- src/View.elm
+++ src/View.elm
@@ -455,7 +455,7 @@ viewPage model =
         , column
             ([ spacing 5, width fill ] ++ Styles.footer)
             [ row [ centerX ]
-                [ text "v0.4.0 | created with "
+                [ text "v0.5.0 | created with "
                 , link Styles.link { url = "http://elm-lang.org/", label = text "Elm" }
                 ]
             , row [ centerX ]
